782ce0b7da2ca7088bfc6f2f01675ff5f091f089
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-10-06 05:52:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const mealRations = [
+  {
+    meal: "breakfast",
+    portions: 1,
+  },
+  {
+    meal: "lunch",
+    portions: 2,
+  },
+];
+
+// res and req are built-in callback functions
+app.get("/meals", (req, res) => {
+  res.send(mealRations);
+});
+
+app.post("/meals", (req, res) => {
+  console.log(req.body);
+  mealRations.push(req.body);
+  res.send(201);
+});
